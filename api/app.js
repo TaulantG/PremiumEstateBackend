c310073f5b95a31f2942dd6f4bcdfe26fa6e964b
TaulantG <ganija.taulant@gmail.com>
2025-09-29 22:08:41 +0200

    crud operation

--- api/app.js
+++ api/app.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
+import userRoute from "./routes/user.route.js";
 
 const app = express();
 
@@ -15,8 +16,9 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/posts", postRoute);
 app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 
 app.listen(8800, ()=>{
