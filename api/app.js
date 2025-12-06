f54ee0abdb5ed2076feb2a14e496096346c5ef4c
TaulantG <ganija.taulant@gmail.com>
2025-12-07 00:21:43 +0100

    controller update

--- api/app.js
+++ api/app.js
@@ -1,4 +1,3 @@
-import 'dotenv/config';
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -6,13 +5,12 @@ import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 
 const app = express();
 
-app.use(cors({ 
-    origin: process.env.CLIENT_URL, 
-    credentials: true 
-}));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -20,7 +18,10 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
+
 
 app.listen(8800, ()=>{
-    console.log("SERVER IS RUNNING!!!");
+    console.log("SERVER IS RUNNING!!!!");
 })
